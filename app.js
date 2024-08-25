40d8fe347c1891b97043e461a62648d521c86970
akanchha-rani <akanchha134@gmail.com>
2024-08-26 01:26:19 +0530

    Update app.js

--- app.js
+++ app.js
@@ -20,8 +20,12 @@ app.get('/login', (req,res) => {
 });
 
 app.get('/profile', isLoggedIn, async (req,res) => {
-    let user = await userModel.findOne({email: req.user.email});
-    user.populate("posts");
+    let user = await userModel.findOne({email: req.user.email}).populate("posts");
+    res.render("profile", {user});
+});
+
+app.get('/profile', isLoggedIn, async (req,res) => {
+    let user = await userModel.findOne({email: req.user.email}).populate("posts");
     res.render("profile", {user});
 });
 
